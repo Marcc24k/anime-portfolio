16656c07a3436afec3452ee87413d1c2ee99db97
Marc Cook <Marc Cook>
2016-04-06 20:48:51 -0400

    added some bugs issues to attack and future plans.

--- js/draggable.js
+++ js/draggable.js
@@ -1,5 +1,11 @@
-//  $(document).ready(function(){
-//                     
-//      $( "#draggable", "#drag").draggable();
-//                          
-//  });
+alert('Good job!');
+
+/***************BUGS TO FIX
+I need this js file to work
+I want to make each item in legend to have infinite # of click copies
+need to have them placed randomly until moved by user
+add some ocean scenery
+
+
+
+*************/
