1ed780d244fc4fcbc125c0ae48c5f56564b86ec2
Marc Cook <Marc Cook>
2016-04-08 19:41:07 -0400

    added some roads

--- js/draggable.js
+++ js/draggable.js
@@ -1,10 +1,11 @@
 alert('Good job!');
 
 /***************BUGS TO FIX
-I need this js file to work
+-I need this js file to work
 I want to make each item in legend to have infinite # of click copies
-need to have them placed randomly until moved by user
-add some ocean scenery
+to have them placed randomly until moved to desired position by user.
+
+-add some ocean scenery
 
 
 
